bb2a9a193d230c09d80420ad7954513809662680
Wander-XIV <aroy1420@gmail.com>
2022-07-11 19:46:43 -0400

    Update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -12,7 +12,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://Wander-XIV.github.io/meet"],
+  redirect_uris: ["https://Wander-XIV.github.io/meet", "http://localhost:8080"],
   javascript_origins: [
     "https://Wander-XIV.github.io",
     "http://localhost:3000",
