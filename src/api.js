fca74ea882cedf028753316cf5b2b372bd075bdb
Wander-XIV <aroy1420@gmail.com>
2022-10-05 12:53:33 -0400

    4.8 updated api

--- src/api.js
+++ src/api.js
@@ -42,17 +42,22 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data ? JSON.parse(data).events : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
     removeQuery();
     const url =
-      "https://naa9acew89.execute-api.us-east-1.amazonaws.com/dev/api/get-events" +
-      "/" +
+      "https://naa9acew89.execute-api.us-east-1.amazonaws.com/dev/api/get-events/" +
       token;
     const result = await axios.get(url);
     if (result.data) {
-      let locations = extractLocations(result.data.events);
+      var locations = extractLocations(result.data.events);
       localStorage.setItem("lastEvents", JSON.stringify(result.data));
       localStorage.setItem("locations", JSON.stringify(locations));
     }
