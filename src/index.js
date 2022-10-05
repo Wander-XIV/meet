afd47a6cb355b070934012907f6c973e981e9005
Wander-XIV <aroy1420@gmail.com>
2022-10-05 12:43:45 -0400

    4.8 updates

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ import * as atatus from "atatus-spa";
 
 atatus.config("bf23199e66f0482cae17df64c9f04618").install();
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
